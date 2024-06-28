f4bc5827c7d124260d0bc10962f6c459cde85299
ilesoviy <ilisovij861@gmail.com>
2024-06-28 09:20:40 +0200

    Moved old tut to ton-hello-world1

--- ton-hello-world1/step9.ts
+++ ton-hello-world1/step9.ts
@@ -7,6 +7,7 @@ async function main() {
   // open wallet v4 (notice the correct wallet version here)
   const mnemonic = "craft one glory creek surround social split donor orchard obscure emerge often cherry attend frequent split torch brother undo leg staff tip vague cattle"; // your 24 secret words (replace ... with the rest of the words)
   const key = await mnemonicToWalletKey(mnemonic.split(" "));
+  console.log('key.publicKey:', key.publicKey);
   const wallet = /* WalletContractV4 */ WalletContractV3R2.create({ publicKey: key.publicKey, workchain: 0 });
 
   // initialize ton rpc client on testnet
